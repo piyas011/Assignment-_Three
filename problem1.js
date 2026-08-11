82826e8744f908e310dcca129a4e6f3c16083dcc
Piyas Ahmed <apiyas761@gmail.com>
2026-08-11 16:57:31 +0600

    update

--- problem1.js
+++ problem1.js
@@ -1,10 +1,22 @@
 const students = "";
 const studentIntroduction = (student) => {
-  if (typeof student !== "object" || student === null) {
+  if (
+    typeof student !== "object" ||
+    student === null ||
+    Array.isArray(student)
+  ) {
     return "Invalid";
   }
 
-  if (!("name" in student) || !("age" in student) || !("course" in student)) {
+  if (
+    !("name" in student) ||
+    !("age" in student) ||
+    !("course" in student) ||
+    typeof student.name !== "string" ||
+    typeof student.age !== "number" ||
+    student.age <= 0 ||
+    typeof student.course !== "string"
+  ) {
     return "Invalid";
   } else {
     return `My name is ${student.name}. I am ${student.age} years old. I am learning ${student.course}.`;
